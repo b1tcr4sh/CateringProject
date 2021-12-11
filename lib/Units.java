0d6facd35d95775d898e83101dba9d3ad46dbe45
Ethan Ford <ethan.ford06@gmail.com>
2021-12-10 21:08:49 -0600

    Added base classes for project structure

---
+++ lib/Units.java
@@ -0,0 +1,3 @@
+public enum Units { // The available unit types that can be used by the ingredients
+    items, tablespoons, teaspoons, cups, slices, oz, pounds
+}
---
+++ src/Recipe.java
@@ -0,0 +1,5 @@
+public class Recipe { // An object to represent an entire recipe, as provided on the project instructions.
+    public String Name; // The name of the recipe.
+    public Ingredients[] Ingredient; // An array of the ingredients required to complete each recipe.
+    public int PeopleServes; // The amount of people that each recipe serves.
+}
