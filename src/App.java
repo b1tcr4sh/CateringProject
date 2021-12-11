ef0c513bea232785be455e72d14ab3d8e4b1d220
Ethan Ford <ethan.ford06@gmail.com>
2021-12-11 01:07:09 -0600

    Added print method to recipe class

--- src/App.java
+++ src/App.java
@@ -2,10 +2,16 @@ import lib.Units;
 
 public class App {
     public static void main(String[] args) {
-        Recipe ChickenParmesian = new Recipe("Chicken Parmessian", new Ingredient[] {new Ingredient("boneless skinless chicken breast halves", 8, Units.items)}, 4);
+        // **This is meant to be an example for Joe to see for the interface.**
 
+        // Creates a new recipe of "Chicken Parmesian" with one ingredient, "boneless skinless chicken breast halves".
+        Recipe ChickenParmesian = new Recipe("Chicken Parmessian", new Ingredient[] {new Ingredient("boneless skinless chicken breast halves", 8, Units.items)}, 4); 
+
+
+        // Calls a method that calculates the amount of ingredients based upon the amount of people that need to be fed.
         Recipe CalculatedChickenParm = Recipe.CalculateIngredients(ChickenParmesian, 5);
         
-        System.out.println(CalculatedChickenParm.Ingredients[0].Quantity);
+        // Prints out a formatted recipe to the console
+        CalculatedChickenParm.PrintRecipe();
     }
 }
--- src/Recipe.java
+++ src/Recipe.java
@@ -1,3 +1,5 @@
+import lib.Units;
+
 public class Recipe { // An object to represent an entire recipe, as provided on the project instructions.
     public String Name; // The name of the recipe.
     public Ingredient[] Ingredients; // An array of the ingredients required to complete each recipe.
@@ -18,5 +20,17 @@ public class Recipe { // An object to represent an entire recipe, as provided on
             calculatedIngredients[i].Quantity = (recipeToModify.Ingredients[i].Quantity / recipeToModify.PeopleServes) * peopleToFeed;
         }
         return new Recipe(recipeToModify.Name, recipeToModify.Ingredients, recipeToModify.PeopleServes); // Returns ne recipe object with calculated ingredient quantities. 
+    }
+
+    public void PrintRecipe() {
+        System.out.println(String.format("Recipe for %s:", Name));
+
+        for (int i = 0; i < Ingredients.length; i++) {
+            if (Ingredients[i].Unit == Units.items) {
+                System.out.println(String.format("%s %s", Ingredients[i].Quantity, Ingredients[i].Name));
+            } else {
+                System.out.println(String.format("%s %s of %s", Ingredients[i].Quantity, Ingredients[i].Unit.toString(), Ingredients[i].Name));
+            }
+        }
     }
 }
