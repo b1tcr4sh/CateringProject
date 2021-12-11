40ead9b245280a3cd9692939463f3452406c95e6
Ethan Ford <ethan.ford06@gmail.com>
2021-12-10 22:59:37 -0600

    Added example code in App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Recipe ChickenParmesian = new Ingredient {
+            Name = "Chicken Parmesian",
+            Ingredients = new Ingredient[] {
+                new Ingredient { Name = "Boneless skinless chicken breast halves", Quantity = 4, Units = Units.items},
+                new Ingredient { Name = "Salt", Quantity = 1, Units = Units.tablespoons}
+            },
+            PeopleServes = 4
+        } 
+
+        Recipe CalculatedChickenParm = Recipe.CalculateIngredients(ChickenParmesian, 5);
+        
+        System.out.println(CalculatedChickenParm.quantity);
     }
 }
--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,5 +1,5 @@
 public class Ingredient { // The attributes used to identify each ingredient
-    public String name; // The name of the ingredient (Can include description ie. Minced, Stale, Toasted, etc.)
+    public String Name; // The name of the ingredient (Can include description ie. Minced, Stale, Toasted, etc.)
     public double Quantity; // The quantity of units that are required of this ingredient
     public Units Unit; // The unit that the quantity refers to.  View Units.java for reference.
 
