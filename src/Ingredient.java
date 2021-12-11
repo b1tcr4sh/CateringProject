e1c6a2aa8b410198498e2767224e8d3b28b967a8
Ethan Ford <ethan.ford06@gmail.com>
2021-12-10 22:48:24 -0600

    Added logic for calculating ingredient amounts

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,6 +1,6 @@
 public class Ingredient { // The attributes used to identify each ingredient
     public String name; // The name of the ingredient (Can include description ie. Minced, Stale, Toasted, etc.)
-    public Int Quantity; // The quantity of units that are required of this ingredient
+    public double Quantity; // The quantity of units that are required of this ingredient
     public Units Unit; // The unit that the quantity refers to.  View Units.java for reference.
 
     public Ingredient ConvertUnits(Units unitToConvertTo) {
--- src/Recipe.java
+++ src/Recipe.java
@@ -1,5 +1,23 @@
 public class Recipe { // An object to represent an entire recipe, as provided on the project instructions.
     public String Name; // The name of the recipe.
-    public Ingredients[] Ingredient; // An array of the ingredients required to complete each recipe.
+    public Ingredient[] Ingredients; // An array of the ingredients required to complete each recipe.
     public int PeopleServes; // The amount of people that each recipe serves.
+
+    public static Recipe CalculateIngredients(Recipe recipe, int peopleToFeed) {
+        Ingredients[] calculatedIngredients = {}; // Initializing a variable that holds a copy of the ingredients of the passed in recipe and does calculations.
+
+        for (int i = 0; i < recipe.Ingredients.Length(); i++) { 
+            // Iterates over the ingredients and scales them to the quantity of people being served.
+            recipe.Ingredients[i].quantity / PeopleServes;
+            recipe.Ingredients[i] = recipe.Ingredients[i] * peopleToFeed;
+
+            // Appends the new ingredient object to the array.
+            calculatedIngredients.Append(recipe.Ingredients[i]);
+        }
+        return new Recipe { // Returns a new recipe object containing the calculated amount.
+            Name = recipe.name,
+            Ingredients = calculatedIngredients,
+            PeopleServes = recipe.PeopleServes,
+        } 
+    }
 }
