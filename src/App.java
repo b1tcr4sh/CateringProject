d8f63b8b656a61a2e3307b675219bd3fb025dc6e
Ethan Ford <ethan.ford06@gmail.com>
2021-12-12 20:34:32 -0600

    Added annotations to explain code

--- src/App.java
+++ src/App.java
@@ -6,46 +6,50 @@ public class App {
     public static void main(String[] args) {
        
 
-     while (true) {
-       
+    while (true) {
+       // Prints messages that define which options are available.
         System.out.println("Please select what you wish to order (By typing the name of the item)");
         System.out.println("1. Chicken Parmesian (Serves 4 people per serving)");
-        System.out.println("2. Chicken Enchiladas (Serves 5 people per serving");
-        System.out.println("3. Meatballs (Serves 8 people per serving");
+        System.out.println("2. Chicken Enchiladas (Serves 5 people per serving)");
+        System.out.println("3. Meatballs (Serves 8 people per serving)");
         
-        Recipe recipeToSend = CollectMenuItem();
+        Recipe recipeToSend = CollectMenuItem(); // Calls the method which Collects the answer and constructs the corresponding recipe.
 
-            System.out.println("How many people are you feeding?");
-            int feedsThisManyPeople = kbScanner.nextInt();
+        System.out.println("How many people are you feeding?"); 
+        int feedsThisManyPeople = kbScanner.nextInt(); // Collects ant int representing the number of people being fed.
 
-            Recipe calculateRecipe = Recipe.CalculateIngredients(recipeToSend, feedsThisManyPeople);
+        // Calls the static method CalculateIngredients on the Recipe class, which calculates the amount of ingredients needed to satisfy the amolunt being served.
+        Recipe calculateRecipe = Recipe.CalculateIngredients(recipeToSend, feedsThisManyPeople); 
 
-            calculateRecipe.PrintRecipe();
-            
-            System.out.println("Would you like to end here? If so type exit!  Click enter to continue.");
-            
-            String s1 = kbScanner.nextLine();
-                if (s1.equals("exit")) {
-                 break;
-                 }
-                 System.out.println("Insert your answer here.");
-              String answer = kbScanner.nextLine();
-              if(answer.equals("exit")){
-                  break;
-              }
-          }
-        kbScanner.close();
+        calculateRecipe.PrintRecipe(); // Calls the PrintRecipe method on the Recipe object, which recurses through each ingredient on the recipe and prints it.
+        
+        // Asks if the user would like to exit the program.
+        System.out.println("Would you like to end here? If so type 'exit'!  Click enter to continue."); 
+        
+        String s1 = kbScanner.nextLine(); // Collects imput from the user, and if it equals "exit", escapes the conditional and reaches end of method.
+            if (s1.equals("exit")) {
+                // If the user enters "exit", then the 
+                break;
+                }
+                System.out.println("Insert your answer here.");
+            String answer = kbScanner.nextLine();
+            if(answer.equals("exit")){
+                break;
+            }
+    }
+        kbScanner.close(); // Releases the Scanner object to system resources, closing the scanning of the standard input stream. 
 
-     }
+    }
 
     private static Recipe CollectMenuItem() {
+        // Collects the input from the user, determines which recipe is being requested, and constructs the appropriate Recipe object.
         String MenuChoice = kbScanner.nextLine();
     
-        Recipe recipeToSend = new Recipe("", new Ingredient[] {}, 0);
+        Recipe recipeToSend = new Recipe("", new Ingredient[] {}, 0); // Defines the recipeToSend Recipe object as an empty recipe to be overwritten by the selected recipe.
         switch(MenuChoice){
             
                 case "Chicken Parmesian": //Chicken Parmesian
-                    recipeToSend = new Recipe("Chicken Parmesian",new Ingredient[] {
+                    recipeToSend = new Recipe("Chicken Parmesian",new Ingredient[] { // Constructs a Recipe object with the properties given in the instructions.
                         new Ingredient("boneless skinless chicken breast halves", 4, Units.items),
                         new Ingredient("salt", 1, Units.tablespoons),
                         new Ingredient("Italian Breadcrumbs", 0.5, Units.cups),
@@ -59,7 +63,7 @@ public class App {
                     break;
                     
                 case "Chicken Enchiladas": //Chicken Enchiladas
-                    recipeToSend = new Recipe("Chicken Enchiladas",new Ingredient[] {
+                    recipeToSend = new Recipe("Chicken Enchiladas",new Ingredient[] { // Constructs a Recipe object with the properties given in the instructions.
                         new Ingredient("Traditional Chicken and Cheese Enchilada Soup", 18.5, Units.oz),
                         new Ingredient("Hot Enchilada Sauce", 10, Units.oz),
                         new Ingredient("Chicken", 1, Units.pounds),
@@ -72,7 +76,7 @@ public class App {
                     break;
                     
                 case "Meatballs": //Meatballs
-                    recipeToSend = new Recipe("Meatballs",new Ingredient[] {
+                    recipeToSend = new Recipe("Meatballs",new Ingredient[] { // Constructs a Recipe object with the properties given in the instructions.
                         new Ingredient("Ground Beef", 1, Units.pounds),
                         new Ingredient("Ground Veal", 0.5, Units.pounds),
                         new Ingredient("Ground Pork", 0.5, Units.pounds),
@@ -88,8 +92,9 @@ public class App {
                     break;
 
                 default:
+                    // In the even the user mistypes their selection, this is called to handle that.
                     System.out.println("Unrecognized Recipe, please try again");
-                    CollectMenuItem();
+                    CollectMenuItem(); // Re-invokes the method, restarting the selection process
                     break;
             }
         return recipeToSend;
